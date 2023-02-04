b4a09dded46f29fc76cab6b689c712e810d0f789
Langerz82 <langley.joshua@gmail.com>
2023-02-04 13:41:33 +1100

    26

--- src/intf/input/sdl/inp_sdl2.cpp
+++ src/intf/input/sdl/inp_sdl2.cpp
@@ -575,7 +575,7 @@ static int JoystickState(int i, int nSubCode)
 		}
 		printf("DPAD %d\n", (dpad_code));
 		int dpad = SDL_JoystickGetButton(JoyList[i], dpad_code);
-		for(var j=0; j < 128; ++j)
+		for(unsigned int j=0; j < 128; ++j)
 			printf("BUTTON %d %d\n", j, SDL_JoystickGetButton(JoyList[i], j));
 
 		if (dpad) 
